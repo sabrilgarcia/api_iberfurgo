ec9e6ad58b1220fbb8f73b5bcb0bf0b027f9b5de
DavidIberfurgo <david.rodrigo@iberfurgo.com>
2022-02-15 15:26:06 +0100

    endpoint menu

---
+++ app/Models/Menu/Menu.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Models\Menu;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Menu extends Model
+{
+    protected $table = 'menus__menu';
+
+
+
+
+
+
+}
---
+++ app/Models/Menu/Submenu.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Models\Menu;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Submenu extends Model
+{
+    protected $table = 'menus__submenu';
+
+
+
+
+
+
+}
