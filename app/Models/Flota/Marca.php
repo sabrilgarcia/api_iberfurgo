fb1b98d87e627d742b9a360da3a0f935221321b3
DavidIberfurgo <david.rodrigo@iberfurgo.com>
2022-03-09 12:16:25 +0100

    solucionado endpoint marcas

--- app/Models/Flota/Marca.php
+++ app/Models/Flota/Marca.php
@@ -3,19 +3,17 @@
 namespace Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Http\Traits\ColumnsNameTrait;
 use Models\Modelo;
 use Models\Ofertas\OfertaVehiculo;
 
 class Marca extends Model
 {
+    use ColumnsNameTrait;
+
     protected $table = 'flota__marca';
 
     //una marca pertenece a varios modelos
-    public function modelo()
-    {
-        return $this->belongsTo(Modelo::class, 'marca_id', 'id');
-    }
-
     public function ofertaVehiculo()
     {
         return $this->hasMany(OfertaVehiculo::class);
